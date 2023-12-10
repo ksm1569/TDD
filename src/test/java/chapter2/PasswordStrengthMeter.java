3eefc6b826b820d7044fc49a5f6fcdcfe65a11c7
ksm1569 <tnals1564@naver.com>
2023-12-10 16:52:48 +0900

    ex2) 암호검사기 - meter 메서드 리펙토링 해보자

--- src/test/java/chapter2/PasswordStrengthMeter.java
+++ src/test/java/chapter2/PasswordStrengthMeter.java
@@ -6,19 +6,22 @@ public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
         if(s == null || s.isEmpty()) return PasswordStrength.INVALID;
 
-        boolean lengthEnough = s.length() >= 8;
-        boolean containsNumer = meetsContainingNumberCriteria(s);
-        boolean containsUpper = meetsContainingUppercaseCriteria(s);
+        int metCounts = getMetCounts(s);
 
-        if(lengthEnough && !containsNumer && !containsUpper) return PasswordStrength.WEAK;
-        if(!lengthEnough && containsNumer && !containsUpper) return PasswordStrength.WEAK;
-        if(!lengthEnough && !containsNumer && containsUpper) return PasswordStrength.WEAK;
-
-        if (!lengthEnough) return PasswordStrength.NORMAL;
-        if (!containsNumer) return PasswordStrength.NORMAL;
-        if (!containsUpper) return PasswordStrength.NORMAL;
+        if (metCounts == 1) return PasswordStrength.WEAK;
+        if (metCounts == 2) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
+    }
+
+    // 조건 만족 갯수를 카운팅 해줌
+    private int getMetCounts(String s) {
+        int metCounts = 0;
+        if (s.length() >= 8) metCounts++;
+        if (meetsContainingNumberCriteria(s)) metCounts++;
+        if (meetsContainingUppercaseCriteria(s)) metCounts++;
+
+        return metCounts;
     }
 
     //숫자 포함체크
